3c1feeb9845e0839f6122d16bc3693c4696f47d7
Pulkit Goyal <pulkit110@gmail.com>
2011-07-02 14:24:20 +0200

    Added events

--- js/TaggerUI.js
+++ js/TaggerUI.js
@@ -1,7 +1,5 @@
 /*
-Copyright 2008-2009 University of Toronto
-Copyright 2008-2009 University of California, Berkeley
-Copyright 2010-2011 OCAD University
+Copyright 2011 OCAD University
 
 Licensed under the Educational Community License (ECL), Version 2.0 or the New
 BSD license. You may not use this file except in compliance with one these
@@ -25,7 +23,7 @@ var fluid_1_4 = fluid_1_4 || {};
 
 (function ($, fluid) {
 
-	var boxes = [];
+	var annotationList = [];
 
 	var canvasElem;
     var canvas;
@@ -40,7 +38,6 @@ var fluid_1_4 = fluid_1_4 || {};
     var rectX;
     var rectY;
     var m_container;
-    var annotationNbUpdater = false;
 
     var mx, my; // mouse coordinates
 
@@ -52,53 +49,65 @@ var fluid_1_4 = fluid_1_4 || {};
 	var strokeStyle;
 	var prevRectIndex = -1;
 	
-
-	function Box() {
-		this.x = 0;
-		this.y = 0;
-		this.w = 1; // default width and height?
-		this.h = 1;
-		this.fill = '#444444';
-		this.tag = "default tag";
-	}
+	var setupAnnotation = function(annotation) {
+		annotation.x = 0;
+		annotation.y = 0;
+		annotation.w = 1; // default width and height?
+		annotation.h = 1;
+		annotation.tag = "default tag";
+	};
 	
-	// New methods on the Box class
-	Box.prototype = {
-		draw: function (context, isFilled) {
-			context.fillStyle = this.fill;
-			context.strokeStyle = strokeStyle;
+	/**
+     * Instantiates a new annotation component.
+     * 
+     * @param {Object} options configuration options for the component.
+     */
+	fluid.defaults("fluid.annotation", {
+		gradeNames: ["fluid.littleComponent", "autoInit"],
+		postInitFunction: "fluid.annotation.postInit",
+		//fillStyle: '#444444',
+		strokeStyle: 'white'
+	});
+	
+	fluid.annotation.postInit = function(that) {
+		setupAnnotation(that);
+		
+		that.draw = function (context, isFilled) {
+			context.fillStyle = that.options.fillStyle;
+			context.strokeStyle = that.options.strokeStyle;
 			// We can skip the drawing of elements that have moved off the screen:
-			if (this.x > WIDTH || this.y > HEIGHT) {
+			if (that.x > WIDTH || that.y > HEIGHT) {
 				return;
 			}
-			if (this.x + this.w < 0 || this.y + this.h < 0) {
+			if (that.x + that.w < 0 || that.y + that.h < 0) {
 				return;
 			}
 			if (isFilled) {
-				context.fillRect(this.x, this.y, this.w, this.h);
+				context.fillRect(that.x, that.y, that.w, that.h);
 			} else {
-				context.strokeRect(this.x, this.y, this.w, this.h);
+				context.strokeRect(that.x, that.y, that.w, that.h);
 			}
-			
-		} // end draw
-	};
-
-	//Initialize a new Box and add it
-	var addRect = function (x, y, w, h, fill, tag) {
-		var rect = new Box();
-		rect.x = x;
-		rect.y = y;
-		rect.w = w;
-		rect.h = h;
-		rect.fill = fill;
-		rect.tag = tag;
-		boxes.push(rect);
+		};
 	};
 	
-	var drawAllBoxes = function (isFilled) {
-		var l = boxes.length;
+	//Initialize a new Box and add it
+	var addAnnotation = function (x, y, w, h, tag, that) {
+		var newAnnotation = fluid.annotation({
+			fillStyle: that.options.fillStyle,
+			strokeStyle: that.options.strokeStyle
+		});
+		newAnnotation.x = x;
+		newAnnotation.y = y;
+		newAnnotation.w = w;
+		newAnnotation.h = h;
+		newAnnotation.tag = tag;
+		annotationList.push(newAnnotation);
+	};
+	
+	var drawAllAnnotations = function (isFilled) {
+		var l = annotationList.length;
 		for (var i = 0; i < l; i++) {
-			boxes[i].draw(context, isFilled);
+			annotationList[i].draw(context, isFilled);
 		}
 	};
 	
@@ -150,162 +159,165 @@ var fluid_1_4 = fluid_1_4 || {};
 	
 	    context.strokeRect(x, y, w, h);	
 	};
-	
-    var taggerMouseMove = function (e) {
-	    
-	    if (!taggerStarted) {
-	        return;
-	    }
-	    
-	    getMouse(e);
 
-	    var x = Math.min(mx, rectX),
-			y = Math.min(my, rectY),
-			w = Math.abs(mx - rectX),
-			h = Math.abs(my - rectY);
-
-		mainDraw(x, y, w, h);	    
-	};
-
-    var taggerMouseUp = function (e) {
-		getMouse(e);
-		if (taggerStarted) {
-			var tag = prompt("Enter any tag");
-			if (tag !== null && tag !== "") {
-
-				var rectH = my - rectY;
-				var rectW = mx - rectX;
-
-				if (rectH < 0) {
-					rectY = my;
-					rectH = -rectH;
-				}
-				if (rectW < 0) {
-					rectX = mx;
-					rectW = -rectW;
-				}
-
-				if (rectW === 0 || rectH === 0) {
-					alert("Error creating tag! Please specify non-zero height and width");
-				} else {
-					addRect(rectX, rectY, rectW, rectH, blurStyle, tag);
-					if (annotationNbUpdater) {
-						annotationNbUpdater(boxes.length);
-					}
-				}
-
-				// Clear the canvas and draw image on canvas
-				context.clearRect(0, 0, canvas.width, canvas.height);
-				drawImage(context, image, resizeFactor);
-			}
-
-			taggerStarted = false;
-			taggerMouseMove(e);
-		}
-	};
-	
-	var annotation = false;
-	var annotationRemove = false;
-	
-		
-	var removeAnnotation = function (i) {
-		boxes.splice(i, 1);
-		if (annotationNbUpdater) {
-			annotationNbUpdater(boxes.length);
-		}
-		canvasElem.mousemove();
-	};
-	
-	var createCrossButton = function (box, index) {
-		var annotationRemove = document.createElement("button");
-		annotationRemove.style.position = 'absolute';
-		annotationRemove.style.top = offsetY + box.y + "px";
-		annotationRemove.style.left = offsetX + box.x + box.w - 20 + "px";
-		annotationRemove.className += ' annotation';
-		annotationRemove.className += ' fl-tagger-annotation-action-remove';
-		
-		annotationRemove.onclick = function () {
-			removeAnnotation(index);
-		};
-		
-		return annotationRemove;
-	};
-	
-	var annotatedMouseMove = function (e) {
-	    getMouse(e);
-
-	    var l = boxes.length;
-	    var i = 0;
-		for (i = 0; i < l; i++) {
-			if (mx > boxes[i].x && mx < boxes[i].x + boxes[i].w &&  my > boxes[i].y && my < boxes[i].y + boxes[i].h) {
-				if (i !== prevRectIndex) {
-					prevRectIndex = i;
-					context.clearRect(0, 0, canvas.width, canvas.height);
-					drawImage(context, image, resizeFactor);
-					boxes[i].draw(context, true);
-					drawAllBoxes(false);
-					
-					// Remove previously shown annotation (important when two annotations overlap)
-					if (annotation) {
-						m_container.get()[0].removeChild(annotation);
-						annotation = false;
-					}
-					if (annotationRemove) {
-						m_container.get()[0].removeChild(annotationRemove);
-						annotationRemove = false;
-					}
-					
-					// Show annotation on mouse over
-					annotation = document.createElement("div");
-					annotation.style.position = 'absolute';
-					annotation.style.top = (offsetY + boxes[i].y) + "px";
-					annotation.style.left = offsetX + boxes[i].x + "px";
-					annotation.style.width = boxes[i].w + 'px';
-					annotation.style.lineHeight = boxes[i].h + 'px';
-					annotation.className += ' fl-tagger-annotation';
-
-					annotation.innerHTML = boxes[i].tag;
-					
-					//Create cross button
-					annotationRemove = createCrossButton(boxes[i], i);
-					
-					m_container.get()[0].appendChild(annotation);
-					m_container.get()[0].appendChild(annotationRemove);
-				}
-				
-				break;
-			}
-		}
-		
-		if (i === l && prevRectIndex !== -1) {
-			context.clearRect(0, 0, canvas.width, canvas.height);
-			drawImage(context, image, resizeFactor);
-			drawAllBoxes(false);
-			prevRectIndex = -1;
-			if (annotation) {
-				m_container.get()[0].removeChild(annotation);
-				annotation = false;
-			}
-			if (annotationRemove) {
-				m_container.get()[0].removeChild(annotationRemove);
-				annotationRemove = false;
-			}
-		}
-	};
-	
     /**
      * Instantiates a new TaggerUI component.
      * 
-     * @param {Object} container the DOM element in which the Image Editor lives
+     * @param {Object} container the DOM element in which the TaggerUI lives
      * @param {Object} options configuration options for the component.
      */
-    fluid.taggerUI = function (container, options) {
-        var that = fluid.initView("fluid.taggerUI", container, options);
-        annotationNbUpdater = that.options.annotationNbUpdater;
-        
-        m_container = container;
-        
-        that.init = function (a_canvas, a_resizeFactor, a_image, a_imageX, a_imageY) {
+    fluid.defaults("fluid.taggerUI", {
+		gradeNames: ["fluid.viewComponent", "autoInit"],
+		postInitFunction: "fluid.taggerUI.postInit",
+		styles: {
+			remove: "fl-image-editor-annotation-action-remove"
+		},
+		status: {
+			remove: "Press Delete key to remove annotation"
+		},
+		events: {
+			onAnnotationNbChange: null,
+		},
+		lineWidth: 1,
+		strokeStyle: 'white',
+		fillStyle: 'rgba(255,255,255,0.4)'
+	});
+    
+    fluid.taggerUI.postInit = function(that) {
+    	m_container = that.container;
+    	
+    	var taggerMouseMove = function (e) {
+	    
+		    if (!taggerStarted) {
+		        return;
+		    }
+		    getMouse(e);
+		    
+		    var x = Math.min(mx, rectX);
+			var y = Math.min(my, rectY);
+			var w = Math.abs(mx - rectX);
+			var h = Math.abs(my - rectY);
+	
+			mainDraw(x, y, w, h);	    
+		};
+	
+	    var taggerMouseUp = function (e) {
+			getMouse(e);
+			if (taggerStarted) {
+				var tag = prompt("Enter any tag");
+				if (tag !== null && tag !== "") {
+					var rectH = my - rectY;
+					var rectW = mx - rectX;
+					if (rectH < 0) {
+						rectY = my;
+						rectH = -rectH;
+					}
+					if (rectW < 0) {
+						rectX = mx;
+						rectW = -rectW;
+					}
+					if (rectW === 0 || rectH === 0) {
+						alert("Error creating tag! Please specify non-zero height and width");
+					} else {
+						addAnnotation(rectX, rectY, rectW, rectH, tag, that);
+						that.events.onAnnotationNbChange.fire(annotationList.length, annotationList.length - 1);
+					}
+
+					// Clear the canvas and draw image on canvas
+					context.clearRect(0, 0, canvas.width, canvas.height);
+					drawImage(context, image, resizeFactor);
+				}
+				taggerStarted = false;
+				taggerMouseMove(e);
+			}
+		};
+	
+		var annotation = false;
+		var annotationRemove = false;
+		
+		var removeAnnotation = function (i) {
+			annotationList.splice(i, 1);
+			that.events.onAnnotationNbChange.fire(annotationList.length, annotationList.length - 1);
+			canvasElem.mousemove();
+		};
+		
+		var createCrossButton = function (box, index) {
+			var annotationRemove = document.createElement("button");
+			annotationRemove.style.position = 'absolute';
+			annotationRemove.style.top = offsetY + box.y + "px";
+			annotationRemove.style.left = offsetX + box.x + box.w - 20 + "px";
+			annotationRemove.className += ' annotation';
+			annotationRemove.className += ' fl-tagger-annotation-action-remove';
+			
+			annotationRemove.onclick = function () {
+				removeAnnotation(index);
+			};
+			
+			return annotationRemove;
+		};
+		
+		var annotatedMouseMove = function (e) {
+		    getMouse(e);
+	
+		    var l = annotationList.length;
+		    var i = 0;
+			for (i = 0; i < l; i++) {
+				if (mx > annotationList[i].x && mx < annotationList[i].x + annotationList[i].w &&  my > annotationList[i].y && my < annotationList[i].y + annotationList[i].h) {
+					if (i !== prevRectIndex) {
+						prevRectIndex = i;
+						context.clearRect(0, 0, canvas.width, canvas.height);
+						drawImage(context, image, resizeFactor);
+						annotationList[i].draw(context, true);
+						drawAllAnnotations(false);
+						
+						// Remove previously shown annotation (important when two annotations overlap)
+						if (annotation) {
+							m_container.get()[0].removeChild(annotation);
+							annotation = false;
+						}
+						if (annotationRemove) {
+							m_container.get()[0].removeChild(annotationRemove);
+							annotationRemove = false;
+						}
+						
+						// Show annotation on mouse over
+						annotation = document.createElement("div");
+						annotation.style.position = 'absolute';
+						annotation.style.top = (offsetY + annotationList[i].y) + "px";
+						annotation.style.left = offsetX + annotationList[i].x + "px";
+						annotation.style.width = annotationList[i].w + 'px';
+						annotation.style.lineHeight = annotationList[i].h + 'px';
+						annotation.className += ' fl-tagger-annotation';
+	
+						annotation.innerHTML = annotationList[i].tag;
+						
+						//Create cross button
+						annotationRemove = createCrossButton(annotationList[i], i);
+						
+						m_container.get()[0].appendChild(annotation);
+						m_container.get()[0].appendChild(annotationRemove);
+					}
+					
+					break;
+				}
+			}
+			
+			if (i === l && prevRectIndex !== -1) {
+				context.clearRect(0, 0, canvas.width, canvas.height);
+				drawImage(context, image, resizeFactor);
+				drawAllAnnotations(false);
+				prevRectIndex = -1;
+				if (annotation) {
+					m_container.get()[0].removeChild(annotation);
+					annotation = false;
+				}
+				if (annotationRemove) {
+					m_container.get()[0].removeChild(annotationRemove);
+					annotationRemove = false;
+				}
+			}
+		};
+    	that.init = function (a_canvas, a_resizeFactor, a_image, a_imageX, a_imageY) {
 
 			canvasElem = a_canvas;
             canvas = a_canvas.get()[0];
@@ -344,15 +356,14 @@ var fluid_1_4 = fluid_1_4 || {};
         };
         
         that.reset = function () {
-			boxes = [];
+        	var oldLength = annotationList.length;
+			annotationList = [];
 			if (canvas) {
 				canvas.onmousedown = null;
 				canvas.onmouseup = null;
 				canvas.onmousemove = null;
 			}
-			if (annotationNbUpdater) {
-				annotationNbUpdater(boxes.length);
-			}
+			that.events.onAnnotationNbChange.fire(annotationList.length, oldLength);
 		};
 		
 		that.doneTagging = function () {
@@ -364,7 +375,7 @@ var fluid_1_4 = fluid_1_4 || {};
 		that.showAnnotations = function () {
 			context.clearRect(0, 0, canvas.width, canvas.height);
 			drawImage(context, image, resizeFactor);
-			drawAllBoxes(false);
+			drawAllAnnotations(false);
 			canvas.onmousemove = annotatedMouseMove;
 		};
 		
@@ -375,7 +386,7 @@ var fluid_1_4 = fluid_1_4 || {};
 		};
 		
 		that.getNbAnnotations = function () {
-			return boxes.length;
+			return annotationList.length;
 		};
 		
 		that.adjustTagsForResize = function (newW, newH, a_resizeFactor, a_image, a_imageX, a_imageY) {
@@ -384,13 +395,13 @@ var fluid_1_4 = fluid_1_4 || {};
 			imageX = a_imageX;
 			imageY = a_imageY;
 			
-		    var l = boxes.length;
+		    var l = annotationList.length;
 		    var i = 0;
 			for (i = 0; i < l; i++) {
-				boxes[i].w *= newW / WIDTH;
-				boxes[i].h *= newH / HEIGHT;
-				boxes[i].x *= newW / WIDTH;
-				boxes[i].y *= newH / HEIGHT;
+				annotationList[i].w *= newW / WIDTH;
+				annotationList[i].h *= newH / HEIGHT;
+				annotationList[i].x *= newW / WIDTH;
+				annotationList[i].y *= newH / HEIGHT;
 			}
 			
 			HEIGHT = newH;
@@ -402,46 +413,28 @@ var fluid_1_4 = fluid_1_4 || {};
 			resizeFactor = a_resizeFactor;
 			imageX = a_imageX;
 			imageY = a_imageY;
-			
-		    var l = boxes.length;
+			var oldLength = annotationList.length; 
+		    var l = annotationList.length;
 		    var i = 0;
 			for (i = 0; i < l; i++) {
-				if (boxes[i].x >= croppingDimensions.x && boxes[i].x + boxes[i].w <= croppingDimensions.x + croppingDimensions.w &&
-						boxes[i].y >= croppingDimensions.y && boxes[i].y + boxes[i].h <= croppingDimensions.y + croppingDimensions.h) {
-					boxes[i].w *= newW / croppingDimensions.w;
-					boxes[i].h *= newH / croppingDimensions.h;
-					boxes[i].x = (boxes[i].x - croppingDimensions.x) * newW / croppingDimensions.w;
-					boxes[i].y = (boxes[i].y - croppingDimensions.y) * newH / croppingDimensions.h;
+				if (annotationList[i].x >= croppingDimensions.x && annotationList[i].x + annotationList[i].w <= croppingDimensions.x + croppingDimensions.w &&
+						annotationList[i].y >= croppingDimensions.y && annotationList[i].y + annotationList[i].h <= croppingDimensions.y + croppingDimensions.h) {
+					annotationList[i].w *= newW / croppingDimensions.w;
+					annotationList[i].h *= newH / croppingDimensions.h;
+					annotationList[i].x = (annotationList[i].x - croppingDimensions.x) * newW / croppingDimensions.w;
+					annotationList[i].y = (annotationList[i].y - croppingDimensions.y) * newH / croppingDimensions.h;
 				} else {
-					boxes.splice(i, 1);
+					annotationList.splice(i, 1);
 					--l;
 					--i;
 				}
 
 			}
-			
-			if (annotationNbUpdater) {
-				annotationNbUpdater(boxes.length);
-			}
+			that.events.onAnnotationNbChange.fire(annotationList.length, oldLength);
 			
 			HEIGHT = newH;
             WIDTH = newW;
 		};
-
-        return that;
-	};
-    
-    fluid.defaults("fluid.taggerUI", {
-		gradeNames: "fluid.viewComponent",
-		styles: {
-			remove: "fl-image-editor-annotation-action-remove"
-		},
-		status: {
-			remove: "Press Delete key to remove annotation"
-		},
-		annotationNbUpdater: false,
-		lineWidth: 1,
-		strokeStyle: 'white'
-	});
+    };
     
 })(jQuery, fluid_1_4);
